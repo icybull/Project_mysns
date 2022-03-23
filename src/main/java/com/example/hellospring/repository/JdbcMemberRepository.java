cc1c7d4c325c610236214fdd2dcbdda3e3e5c5e0
icybull <rlacksdn4564@naver.com>
2022-03-23 11:19:44 +0900

    spring222

---
+++ src/main/java/com/example/hellospring/repository/JdbcMemberRepository.java
@@ -0,0 +1,40 @@
+package com.example.hellospring.repository;
+
+import com.example.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+
+public class JdbcMemberRepository implements MemberRepository{
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
